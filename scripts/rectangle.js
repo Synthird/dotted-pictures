e8e74f384686c438a27c6bcdba3c8e5dd127bd15
Synthird <105394020+Synthird@users.noreply.github.com>
2025-03-02 23:11:19 -0800

    Add files via upload

---
+++ scripts/rectangle.js
@@ -0,0 +1,36 @@
+const createRectangle = document.getElementById("create-rectangle");
+
+const widthBox = document.getElementById("width-box");
+const heightBox = document.getElementById("height-box");
+
+const rectangle = document.getElementById("rectangle");
+
+let width = "";
+let height = "";
+
+function drawRectangle() {
+	rectangle.textContent = "";
+
+	for (let i = 0; i < widthBox.value; i++) {
+		width = `${width}.`;
+	}
+
+	for (let i = 0; i < heightBox.value; i++) {
+		rectangle.textContent += `\n${width}`;
+	}
+
+	width = "";
+	height = "";
+}
+
+function drawOnEnterPressed(event) {
+	switch (event.key) {
+		case "Enter":
+			drawRectangle();
+			break;
+	}
+}
+
+createRectangle.addEventListener("click", drawRectangle);
+widthBox.addEventListener("keyup", drawOnEnterPressed);
+heightBox.addEventListener("keyup", drawOnEnterPressed);
---
+++ scripts/skyscraper.js
@@ -0,0 +1,33 @@
+const createSkyscraper = document.getElementById("create-skyscraper");
+
+const floorsBox = document.getElementById("floors-box");
+const rodsBox = document.getElementById("rods-box");
+
+const skyscraper = document.getElementById("skyscraper");
+
+const rodTemplate = "\n.";
+const floorTemplate = "\n.......";
+
+function drawSkyscraper() {
+	skyscraper.textContent = "";
+
+	for (let i = 0; i < rodsBox.value; i++) {
+		skyscraper.textContent += rodTemplate;
+	}
+
+	for (let i = 0; i < floorsBox.value; i++) {
+		skyscraper.textContent += floorTemplate;
+	}
+}
+
+function drawOnEnterPressed(event) {
+	switch (event.key) {
+		case "Enter":
+			drawSkyscraper();
+			break;
+	}
+}
+
+createSkyscraper.addEventListener("click", drawSkyscraper);
+floorsBox.addEventListener("keyup", drawOnEnterPressed);
+rodsBox.addEventListener("keyup", drawOnEnterPressed);
