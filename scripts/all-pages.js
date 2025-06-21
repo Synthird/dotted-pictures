d4e6b5008ee636a38a6e60f3dfbcbe5f1bb0d02d
Synthird <105394020+Synthird@users.noreply.github.com>
2025-06-20 17:17:38 -0700

    Adding an event for clicking the draw button is done before adding an event for pressing enter

--- scripts/all-pages.js
+++ scripts/all-pages.js
@@ -1,17 +1,17 @@
 const controls = document.querySelector(".control-panel");
 
 export function drawOnEventTrigger(drawFunc) {
-	controls.addEventListener("keyup", event => {
-		switch (event.key) {
-			case "Enter":
+	controls.addEventListener("click", event => {
+		switch (event.target.getAttribute("id")) {
+			case "draw":
 				drawFunc();
 				break;
 		}
 	});
 
-	controls.addEventListener("click", event => {
-		switch (event.target.getAttribute("id")) {
-			case "draw":
+	controls.addEventListener("keyup", event => {
+		switch (event.key) {
+			case "Enter":
 				drawFunc();
 				break;
 		}
